5d31229f3c531068004a3bd701b4cdc0636a475c
tankho0221-png <tankho0221@gmail.com>
2026-09-16 20:38:29 +0800

    Build and test curriculum competition modules

--- _teaching-system/scripts/test.cjs
+++ _teaching-system/scripts/test.cjs
@@ -1,6 +1,6 @@
 const {spawnSync}=require('node:child_process');
 const path=require('node:path');
-for(const name of ['test-learning.cjs','test-interactive.cjs','test-online.cjs','test-build.cjs','test-bridge.cjs']) {
+for(const name of ['test-competition.cjs','test-learning.cjs','test-interactive.cjs','test-online.cjs','test-build.cjs','test-bridge.cjs']) {
   const result=spawnSync(process.execPath,[path.join(__dirname,'../tests',name)],{stdio:'inherit'});
   if(result.status!==0) process.exit(result.status||1);
 }
